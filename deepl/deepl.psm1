3c151db853ff891b220adc27ed920ab2c89fb903
Christopher Hill <chill.signup@gmail.com>
2021-04-20 09:06:31 +0200

    Added usage and support languages functions

--- deepl/deepl.psm1
+++ deepl/deepl.psm1
@@ -154,53 +154,117 @@
    Get-DeeplTranslation -DeeplAuthKey "xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" -InputText "Привет мир" -SourceLanguage "RU" -TargetLanguage "NL" -Formality "more" -TranslatedTextOnly $false
 
 #>
-function Get-DeeplTranslation {
+
+function Set-DeeplUri {
     param (
-        [Parameter(Mandatory = $true)][String]$DeeplAuthKey,
-        [Parameter(Mandatory = $true)][string]$InputText,
-        [Parameter(Mandatory = $false)][String]$TargetLanguage = "EN", # Default is English
+        [Parameter(Mandatory = $false)][string]$APIMode
+    )
+    Return "https://api$($APIMode).deepl.com/v2"
+}
+function Get-DeeplTextTranslation {
+    param (
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][String]$DeeplAuthKey,
+        [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string]$InputText,
+        [Parameter(Mandatory = $false)][ValidateNotNullOrEmpty()][String]$TargetLanguage = "EN", # Default is English
         [Parameter(Mandatory = $false)][String]$SourceLanguage,
-        [Parameter(Mandatory = $false)][String]$SplitSentences = "1",
-        [Parameter(Mandatory = $false)][String]$PreserverFormatting = "0",
-        [Parameter(Mandatory = $false)][String]$Formality= "default",
+        [Parameter(Mandatory = $false)][ValidateSet("0", "1", "nonewlines")][String]$SplitSentences = "1",
+        [Parameter(Mandatory = $false)][ValidateSet("0", "1")][String]$PreserverFormatting = "0",
+        [Parameter(Mandatory = $false)][ValidateSet("default", "more", "less")][String]$Formality = "default",
         [Parameter(Mandatory = $false)][String]$TagHandling,
         [Parameter(Mandatory = $false)][String]$NonSplittingTags,
         [Parameter(Mandatory = $false)][String]$OutlineDetection,
         [Parameter(Mandatory = $false)][String]$SplittingTags,
         [Parameter(Mandatory = $false)][String]$IgnoreTags,
         [Parameter(Mandatory = $false)][Bool]$TranslatedTextOnly = $true,
-        [Parameter(Mandatory = $false)][ValidateSet($null,"-free")][string]$APIMode = $null
+        [Parameter(Mandatory = $false)][ValidateSet($null, "-free")][string]$APIMode = $null
     )
 
     $DeeplRequestBody = @{
-        auth_key    = $DeeplAuthKey
-        text        = $InputText
+        auth_key            = $DeeplAuthKey
+        text                = $InputText
         # text        = $InputText2  <-- how to add multiple text objects? hashtables only allow 1 unique keyvalue
-        target_lang = $TargetLanguage
-        source_lang = $SourceLanguage
-        split_sentences = $SplitSentences
+        target_lang         = $TargetLanguage
+        source_lang         = $SourceLanguage
+        split_sentences     = $SplitSentences
         preserve_formatting = $PreserverFormatting
-        formality = $Formality
-        tag_handling = $TagHandling
-        non_splitting_tags = $NonSplittingTags
-        outline_detection = $OutlineDetection
-        splitting_tags = $SplittingTags
-        ignore_tags = $IgnoreTags
+        formality           = $Formality
+        tag_handling        = $TagHandling
+        non_splitting_tags  = $NonSplittingTags
+        outline_detection   = $OutlineDetection
+        splitting_tags      = $SplittingTags
+        ignore_tags         = $IgnoreTags
     }
+    $Uri = Set-DeeplUri $APIMode
     try {
-        $Response = Invoke-WebRequest -Method POST -Uri "https://api$($APIMode).deepl.com/v2/translate" -body $DeeplRequestBody -ContentType "application/x-www-form-urlencoded" -ErrorAction Stop
+        $Response = Invoke-WebRequest -Method POST -Uri "$($Uri)/translate" -body $DeeplRequestBody -ContentType "application/x-www-form-urlencoded" -ErrorAction Stop
     }
     catch [System.Net.WebException] { 
         $_.Exception.Message
     }
     if ($Response.StatusCode -eq 200) {
         if ($TranslatedTextOnly) {
-        $TranslatedText = ($Response.Content | ConvertFrom-Json).translations.text
-        Return $TranslatedText
+            $TranslatedText = ($Response.Content | ConvertFrom-Json).translations.text
+            Return $TranslatedText
         }
         else {
             Return $Response.Content
         }
     }
 }
-Export-ModuleMember -Function Get-DeeplTranslation
+
+function Get-DeeplUsage {
+    param (
+        [Parameter(Mandatory = $true)][String]$DeeplAuthKey,
+        [Parameter(Mandatory = $false)][ValidateSet($null, "-free")][string]$APIMode = $null
+    )
+    $Uri = Set-DeeplUri $APIMode
+
+    $DeeplRequestBody = @{
+        auth_key    = $DeeplAuthKey
+        target_lang = $TargetLanguage
+    }
+
+    try {
+        $Response = Invoke-WebRequest -Method POST -Uri "$($Uri)/usage" -body $DeeplRequestBody -ContentType "application/x-www-form-urlencoded" -ErrorAction Stop
+    }
+    catch [System.Net.WebException] { 
+        $_.Exception.Message
+    }
+    if ($Response.StatusCode -eq 200) {
+        Return $Response.Content
+    }
+}
+function Get-DeeplSupportedLanguages {
+    param (
+        [Parameter(Mandatory = $true)][String]$DeeplAuthKey,
+        [Parameter(Mandatory = $false)][ValidateSet("source", "target")][String]$Type,
+        [Parameter(Mandatory = $false)][ValidateSet($null, "-free")][string]$APIMode = $null
+    )
+    $Uri = Set-DeeplUri $APIMode
+
+    $DeeplRequestBody = @{
+        auth_key = $DeeplAuthKey
+        type     = $type
+    }
+
+    try {
+        $Response = Invoke-WebRequest -Method POST -Uri "$($Uri)/languages" -body $DeeplRequestBody -ContentType "application/x-www-form-urlencoded" -ErrorAction Stop
+    }
+    catch [System.Net.WebException] { 
+        $_.Exception.Message
+    }
+    if ($Response.StatusCode -eq 200) {
+        Return $Response.Content
+    }
+}
+
+Export-ModuleMember -Function Get-DeeplTextTranslation
+Export-ModuleMember -Function Get-DeeplUsage
+Export-ModuleMember -Function Get-DeeplSupportedLanguages
+
+# Todo
+# function TranslateDocuments {
+#    UploadDocument
+#    CheckStatus
+#    Downloading
+# }
